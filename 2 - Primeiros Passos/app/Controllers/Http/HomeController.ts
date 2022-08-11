71317300371c806d0cc240a9ee61acbf730a3356
Bruno Alves Vieira <94388068+I3run01@users.noreply.github.com>
2022-08-11 08:55:46 -0300

    c

--- 2 - Primeiros Passos/app/Controllers/Http/HomeController.ts
+++ 2 - Primeiros Passos/app/Controllers/Http/HomeController.ts
@@ -1,3 +1,10 @@
 // import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
-export default class HomeController {}
+export default class HomeController {
+
+    index({view}) {
+        
+    }
+
+
+}

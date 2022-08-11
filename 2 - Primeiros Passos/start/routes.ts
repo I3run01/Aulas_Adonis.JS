1e6cf1369be753399a8019bdfe50374accb786f9
Bruno Alves Vieira <94388068+I3run01@users.noreply.github.com>
2022-08-11 08:38:43 -0300

    c

--- 2 - Primeiros Passos/start/routes.ts
+++ 2 - Primeiros Passos/start/routes.ts
@@ -1,29 +1,6 @@
-/*
-|--------------------------------------------------------------------------
-| Routes
-|--------------------------------------------------------------------------
-|
-| This file is dedicated for defining HTTP routes. A single file is enough
-| for majority of projects, however you can define routes in different
-| files and just make sure to import them inside this file. For example
-|
-| Define routes in following two files
-| ├── start/routes/cart.ts
-| ├── start/routes/customer.ts
-|
-| and then import them inside `start/routes.ts` as follows
-|
-| import './routes/cart'
-| import './routes/customer''
-|
-*/
-
 import Route from '@ioc:Adonis/Core/Route'
-import Env from '@ioc:Adonis/Core/Env' //Biblioteca para ativar o auto complete
 
 Route.get('/', async ({ view }) => {
   return view.render('welcome')
-
-  Env.get('EMAIL_USER')
 
 })

81ecf9ce541cbb559222450b1ddffd3f5d0eee75
Bruno Alves Vieira <94388068+I3run01@users.noreply.github.com>
2022-08-16 08:50:06 -0300

    c

--- 3 - Rotas e Router/start/api.routes.ts
+++ 3 - Rotas e Router/start/api.routes.ts
@@ -5,7 +5,17 @@ Route.group(() => {
 
     Route.group(()=> {
         Route.get('/', 'PainelsController.index')
+
         Route.get('/usuarios/', 'PainelsController.usuarios')
+
+        Route.get('/usuario/:id?/', 'PainelsController.usuarioByID')
+            .where('id', Route.matchers.number())//Vai aceitar só numeros
+            //Se tiver o ponto de interrogação (?), o parâmetro passa a ser opcional
+
+        Route.get('/usuario/:slug', 'PainelsController.usuarioBySlug')
+            .where('slug', Route.matchers.slug())
+
+        Route.get('/docs/*', 'PainelsController.docs')
     }).prefix('/painels/')
     
 }).prefix('/api') //agora todas as rotas tem o prefixo api
